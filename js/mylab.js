d7a42a8ff4e121c295b5b04ad7085cb12928a6a9
Hamad <hamadali77115033.com>
2018-10-27 08:57:33 -0700

    Roll no 04471 Name Muhammad Hamad

--- js/mylab.js
+++ js/mylab.js
@@ -14,10 +14,25 @@
  }
 
  var studentArray = JSON.parse(localStorage.getItem("Student Data")) || [];
+ var selectedindex = -1;
 
  function dataSubmit() {
-     event.preventDefault();
-     var student = new Studentdata();
+
+
+     var student = localStorage.getItem('editObject');
+
+     if (student) {
+         student = JSON.parse(student);
+
+
+     } else {
+
+         student = new Studentdata();
+         student.id = Math.round(Math.random() * 100);
+
+     }
+
+
      student.StudentName = stuName.value;
      student.FatherName = stufname.value;
      student.StudentCnic = CNIC.value;
@@ -25,42 +40,118 @@
      student.city = typeselectt.value;
      student.Address = Address.value;
      student.gender = document.querySelector(".Gender:checked").value;
-     studentArray.push(student);
+
+     
+     var tElement = studentArray.filter(function(sStudent){
+            return sStudent.id == student.id;
+     })[0];
+
+     if(tElement){
+         var eIndex = studentArray.indexOf(tElement);
+         studentArray[eIndex] = student;
+         localStorage.removeItem('editObject');
+     }else{
+         studentArray.push(student);
+     }
+
+
+
+
+     //  if (selectedindex === -1) {
+
+     //      studentArray.push(student);
+     //  } else {
+     //      console.log("SsS");
+     //      studentArray.splice(selectedindex, 1, student);
+     //  }
      localStorage.setItem("Student Data", JSON.stringify(studentArray));
 
 
 
+
+
+ }
+
+
+ let editObejct = (index) => {
+
+
+     selectedindex = index;
+     var updatedate = studentArray[index];
+     localStorage.setItem('editObject', JSON.stringify(updatedate));
+     location.href = 'index.html';
 
  }
 
  let itemupdate = (index) => {
-    var stor= JSON.parse(localStorage.getItem("Student Data"))
-   var gett =    stor[index];
-   stuName.value   =  gett.StudentName;
+
+
+     let editignObject = localStorage.getItem('editObject');
+
+     if (!editignObject) {
+         return
+     }
+
+
+     editignObject = JSON.parse(editignObject)
+
+     var updatedate = editignObject
+
+     stuName.value = updatedate.StudentName;
+
+     stufname.value = updatedate.FatherName
+     CNIC.value = updatedate.StudentCnic;
+     datecurrent.value = updatedate.SubmittDate;
+     typeselectt.value = updatedate.city;
+     Address.value = updatedate.Address;
+     document.querySelector(".Gender:checked").value = updatedate.gender;
+     document.getElementById("Submitt").innerHTML = "Update";
+     myfuntion();
+
+
  }
 
  let itemDelete = (index) => {
+
      studentArray.splice(index, 1);
      localStorage.setItem("Student Data", JSON.stringify(studentArray));
-     
+     myfuntion();
+
  }
 
-//   function myFunction(index){
-//     console.log(in)
-//     // var stor= JSON.parse(localStorage.getItem("Student Data")) ;
-//     //  var gett =stor[index];
-//     //  console.log(gett);
 
 
 
-//  }
 
 
- studentArray.map((item,index) => {
-      //console.log('index:',item);
-
-     dis.innerHTML +=`<div>Name : ${item.StudentName} FatherName: ${item.FatherName} <button onclick=itemDelete(${index})> Delete </button> <button onclick=itemupdate(${index})> Update </button> <div/>`;
+ function myfuntion() {
+     var studentArray = JSON.parse(localStorage.getItem("Student Data")) || [];
 
 
- });
+     dis.innerHTML = "";
+     studentArray.map((item, index) => {
+         //console.log('index:',item);
 
+         dis.innerHTML += `<div class="container"><table class="striped" id="myid"><thead id="tblhead"><tr></tr><th>Name</th><th>Father Name</th> <th>Student Cnic </th>
+     <th>Submitt Date</th>
+     <th>city</th>
+     <th>Address</th>
+     <th>Gender</th>
+     <th>Opertion</th>
+
+     
+ </tr></thead> <tbody id="tblbody"><tr>
+ <td>${item.StudentName}</td>
+ <td>${item.FatherName}</td>
+ <td>${item.StudentCnic}</td>
+ <td>${item.SubmittDate}</td>
+ <td>${item.city}</td>
+ <td>${item.Address}</td>
+ <td>${item.gender}</td>
+ 
+ 
+ <td><div><button onclick=itemDelete(${index}) class="waves-effect waves-light btn"> Delete </button>&nbsp;&nbsp;<button onclick=editObejct(${index}) class="waves-effect waves-light btn"> Update </button></td></tr></tbody>  </table><div/>`;
+
+
+     });
+ }
